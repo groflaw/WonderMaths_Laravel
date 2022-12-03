fb19de0669d08fd07c1a6046065e981658c6424a
Groflaw <groflaw.eros@gmail.com>
2022-12-03 07:35:12 +0700

    promo3

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('lead_id');
+            $table->string('product_code_id');
+            $table->string('name');
+            $table->string('contact_number');
+            $table->string('email')->unique();
+            $table->string('location');
+            $table->string('status');
+            $table->integer('role_id');
+            $table->string('bank');
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
